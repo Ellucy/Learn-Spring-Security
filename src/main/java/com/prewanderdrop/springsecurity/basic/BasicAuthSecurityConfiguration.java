357f255c83e979e48fc822ae3442c425ab22ed61
Eleri Mets <eleri.apsolon@gmail.com>
2024-02-24 00:04:14 +0200

    Launch a H2 in-memory database and execute scripts to create schema. Store credentials to a database using JDBC.

--- src/main/java/com/prewanderdrop/springsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/prewanderdrop/springsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.StaticAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,6 +28,12 @@ public class BasicAuthSecurityConfiguration {
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .headers((headers) ->
+                        headers
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
         return http.build();
     }
 }
